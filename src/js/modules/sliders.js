f5217c50b83468ecf2a45248248b894787abf472
yuriikaplunivskyi <yurii.kaplunivskyi@gmail.com>
2023-08-16 00:08:26 +0300

    feat: slider

--- src/js/modules/sliders.js
+++ src/js/modules/sliders.js
@@ -1,9 +1,8 @@
 const sliders = (slides, dir, prev, next) => {
-    let slideIndex = 1;
+    let slideIndex = 1,
+        paused = false;
 
-    const items = document.querySelectorAll(slides),
-          prevBtn = document.querySelector(prev),
-          nextBtn = document.querySelector(next);
+    const items = document.querySelectorAll(slides)
     
     function showSlides (n) {
         if (n > items.length) {
@@ -28,6 +27,51 @@ const sliders = (slides, dir, prev, next) => {
         showSlides(slideIndex += n);
     }
 
+    try {
+        const prevBtn = document.querySelector(prev),
+              nextBtn = document.querySelector(next);
+
+        prevBtn.addEventListener('click', () => {
+            plusSlides(-1);
+            items[slideIndex - 1].classList.remove('slideInLeft');
+            items[slideIndex - 1].classList.add('slideInRight');
+        });
+
+        nextBtn.addEventListener('click', () => {
+            plusSlides(1);
+            items[slideIndex - 1].classList.remove('slideInRight');
+            items[slideIndex - 1].classList.add('slideInLeft');
+        });
+    }
+    catch(e) {
+
+    }
+
+    function activateAnimation () {
+        if (dir === 'vertical') {
+            paused = setInterval(function () {
+                plusSlides(1);
+                items[slideIndex - 1].classList.add('slideInDown');
+            }, 3000)
+        } else {
+            paused = setInterval(function () {
+                plusSlides(1);
+                items[slideIndex - 1].classList.remove('slideInRight');
+                items[slideIndex - 1].classList.add('slideInLeft');
+            }, 3000)
+        }
+    }
+
+    activateAnimation();
+
+    items[0].parentNode.addEventListener('mouseenter', () => {
+        clearInterval(paused);
+    })
+
+    items[0].parentNode.addEventListener('mouseleave', () => {
+        activateAnimation();
+    })
+
 }
 
-export default sliders;
+export default sliders; 
